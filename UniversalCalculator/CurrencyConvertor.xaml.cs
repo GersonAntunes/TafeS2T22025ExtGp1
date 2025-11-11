caa6555cef976bf818eec93b9472e3e4780f9ce2
Ben Cuttance <bcuttance1998@gmail.com>
2025-11-11 21:25:55 +1030

    FEAT(currencyCalc): Added basic page layout. Methods added to change textblock to display amount of money and currency selected

---
+++ UniversalCalculator/CurrencyConvertor.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Calculator
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class CurrencyConvertor : Page
+	{
+		public CurrencyConvertor()
+		{
+			this.InitializeComponent();
+		}
+
+		private void AmountInput_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			string amount = AmountInput.Text;
+
+
+			if (amount == "")
+			{
+				amount = "100";
+			}
+
+			CurrencyAmount.Text = amount;
+		}
+		private void FromCurrencyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (FromCurrencyComboBox.SelectedItem is ComboBoxItem selectedItem)
+			{
+				string currency = selectedItem.Content.ToString();
+				CurrencyType.Text = currency + " =";
+			}
+		}
+	}
+
+}
