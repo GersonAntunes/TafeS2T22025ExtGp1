32e28066b10c5a7e269faecb433b59586263fcb5
001046779 <isaac.brown2@student.tafesa.edu.au>
2025-11-20 10:41:32 +1030

    Initial coding and styling for UnitConverter done. no building or error checking has been done yet. still need to reference UnitConverter for MainMenu

--- UniversalCalculator/UnitConverter.xaml.cs
+++ UniversalCalculator/UnitConverter.xaml.cs
@@ -26,5 +26,75 @@ namespace Calculator
 		{
 			this.InitializeComponent();
 		}
+
+		private void temperatureButton_Click(object sender, RoutedEventArgs e)
+		{
+			double temp = int.Parse(inputTextBox.Text);
+			if(title.Text == "Metric to Imperial")
+			{
+				temp = (temp * 1.8) + 32;
+			}
+			else if(title.Text == "Imperial to Metric")
+			{
+				temp = (temp - 32) / 1.8;
+			}
+			outputTextBox.Text = temp.ToString();
+
+		}
+
+		private void distanceButton_Click(object sender, RoutedEventArgs e)
+		{
+			double dist = int.Parse(inputTextBox.Text);
+			if(title.Text == "Metric to Imperial")
+			{
+				dist = dist * 3.28084;
+			}
+			else if(title.Text == "Imperial to Metric")
+			{
+				dist = dist / 3.28084;
+			}
+			outputTextBox.Text = dist.ToString();
+		}
+
+		private void massButton_Click(object sender, RoutedEventArgs e)
+		{
+			double mass = int.Parse(inputTextBox.Text);
+			if(title.Text == "Metric to Imperial")
+			{
+				mass = mass * 2.20462;
+			}
+			else if(title.Text == "Imperial to Metric")
+			{
+				mass = mass / 2.20462;
+			}
+			outputTextBox.Text = mass.ToString();
+		}
+
+		private void pressureButton_Click(object sender, RoutedEventArgs e)
+		{
+			double press = int.Parse(inputTextBox.Text);
+			if(title.Text == "Metric to Imperial")
+			{
+				press = press * 0.14503;
+			}
+			else if(title.Text == "Imperial to Metric")
+			{
+				press = press / 0.14503;
+			}
+			outputTextBox.Text = press.ToString();
+		}
+
+		private void switchButton_Click(object sender, RoutedEventArgs e)
+		{
+			if(title.Text == "Metric to Imperial")
+			{
+				title.Text = "Imperial to Metric";
+			}
+			else if(title.Text == "Imperial to Metric")
+			{
+				title.Text = "Metric to Imperial";
+			}
+
+		}
 	}
 }
